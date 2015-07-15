7e42d5d5ac20fdc9036299e871df8c238af5865f
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2015-07-14 20:56:18 -0400

    Add Jenkins widget to Flow.
    This currently doesn't render, for reasons I don't understand.

---
+++ server/lib/services/built-in/jenkins/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var _       = require('lodash'),
+    request = require('request');
+    
+function Provider(options) {
+    this.options = options;
+}
+
+Provider.prototype.fetch = function(done) {
+    var colorMap = {
+        'red':      'failure',
+        'blue':     'success',
+        'white':    'success',   // this is actually 'running'
+        'aborted':  'failure',
+        'disabled': 'success',
+        'yellow':   'success'   // warning
+    };
+    var jobs = [];
+    request.get({
+        url: this.options.url,
+        json: true
+    }, function (error, response, body) {
+        var jobs = _(body.jobs).countBy(function (job) {
+            return colorMap[job.color]
+        }).value()
+        var data = {
+            value: jobs.success, 
+            total: jobs.success + jobs.failure
+        }
+        console.log(data);
+        done(null, data);
+    });
+
+};
+
+module.exports = Provider;
